0ad18a913a4eccb0fa7d512733e9fe43edfc9ea0
Daniel Marques <daniel.marques142@gmail.com>
2025-09-21 15:51:14 -0300

    feat: allow incoming traffic

--- lib/cdk-deploy-ec2-stack.ts
+++ lib/cdk-deploy-ec2-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, Vpc } from 'aws-cdk-lib/aws-ec2'
+import { Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, Vpc, Port } from 'aws-cdk-lib/aws-ec2'
 
 export class CdkDeployEc2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -24,5 +24,7 @@ export class CdkDeployEc2Stack extends cdk.Stack {
         instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),        
       }
     )
+
+    instance.connections.allowFromAnyIpv4(Port.tcp(80))
   }
 }
